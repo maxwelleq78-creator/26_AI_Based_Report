e771cb2b0467f40140c01ec02b94666990a8c9f6
maxwelleq78-creator <maxwelleq78@gmail.com>
2026-09-08 11:45:29 +0900

    Refresh PWA cache for v1.2

--- barcelona-gps-guide/sw.js
+++ barcelona-gps-guide/sw.js
@@ -1,5 +1,5 @@
-const CACHE="bcn-gps-guide-v1.1-ui";
+const CACHE="bcn-gps-guide-v1.2";
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>cached)))});
